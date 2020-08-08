859fc4d875235e44d15e50c5b8e58b95af690e65
henry <>
2020-08-08 10:17:41 +0800

    update2020-8-8

---
+++ 前端/react/kkb-react-20-/lesson4/src/k-react-router-dom/Prompt.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { RouterContext } from "./Context";
+
+//暗号：尼日尔
+export default function Prompt({ message, when = true }) {
+  return (
+    <RouterContext.Consumer>
+      {context => {
+        if (!when) {
+          return null;
+        }
+        let method = context.history.block;
+        return (
+          <LifeCycle
+            onMount={self => {
+              self.release = method(message);
+            }}
+            onUnmount={self => {
+              self.release();
+            }}
+          />
+        );
+      }}
+    </RouterContext.Consumer>
+  );
+}
+
+
+class LifeCycle extends Component {
+
+  componentDidMount() {
+    if (this.props.onMount) {
+      this.props.onMount.call(this, this);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.onUnmount) {
+      this.props.onUnmount.call(this, this);
+    }
+  }
+  render() {
+    return null;
+  }
+}
